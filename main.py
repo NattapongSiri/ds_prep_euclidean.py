83bb34590afc17c6d7773f1f150140d7b8b43ec0
Nattapong Sirilappanich <s.nattapong@gmail.com>
2018-11-24 13:06:04 +0700

    Turn ast.literal_eval into json.loads to make it faster

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from itertools import islice, tee
 import json
 from math import sqrt
 import time
+from numba import float64, int32, jit
 
-def _euc_dist(p1, p2):
+@jit(float64(int32, int32, int32, int32, int32, int32), nopython=True, cache=True)
+def _euc_dist(x1, y1, z1, x2, y2, z2):
     return sqrt(
-        pow(p1[0] - p2[0], 2) +
-        pow(p1[1] - p2[1], 2) +
-        pow(p1[2] - p2[2], 2)
+        pow(x1 - x2, 2) +
+        pow(y1 - y2, 2) +
+        pow(z1 - z2, 2)
     )
 
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
 start_time = time.perf_counter()
-with open("data.txt", "r") as data_src:
-    with open("processed.txt", "w") as data_sink:
+with open("data.txt", "r", buffering=32*2**20) as data_src:
+    with open("processed.txt", "w", buffering=32*2**20) as data_sink:
         n = 5
-        val = (_euc_dist(pair[0][i], pair[1][i]) for pair in pairwise((ast.literal_eval(line) for line in data_src)) for i in range(0, n))
-        dist = []
-        for v in val:
-            dist.append(v)
+        records = []
+        records.append(json.loads(next(data_src)))
+        records.append(json.loads(next(data_src)))
+        distance = [0 for i in range(0, n)]
+
+        for line in data_src:
+            for i in range(0, n):
+                distance[i] = _euc_dist(records[0][i][0], records[0][i][1], records[0][i][2], records[1][i][0], records[1][i][1], records[1][i][2])
+            data_sink.write("%s" % line)
+            records[0] = records[1]
+            records[1] = json.loads(line)
             
-            if len(dist) == 5:
-                data_sink.write("%s\n" % dist)
-                dist.clear()
                 
 print("Done in", time.perf_counter() - start_time, "s")
